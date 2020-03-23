13b0c45ca9f5c66437e12b4fc16e4e151f580376
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-03-23 17:06:44 +0100

    added types definition

--- cjs/index.js
+++ cjs/index.js
@@ -26,11 +26,23 @@ const uglifyArgs = {output: {comments: /^!/}};
 
 const SVGO = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('svgo'));
 
+/**
+ * Create a file after minifying or optimizing it, when possible.
+ * @param {string} source The source file to optimize.
+ * @param {string} dest The optimized destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 const ucompress = (source, dest) => {
   const method = extname(source).toLowerCase().slice(1);
   return (ucompress[method] || ucompress.copy)(source, dest);
 };
 
+/**
+ * Create a file after minifying via `csso`.
+ * @param {string} source The source CSS file to minify.
+ * @param {string} dest The minified destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.css = (source, dest) => new Promise((res, rej) => {
   readFile(source, (err, data) => {
     if (err)
@@ -44,18 +56,36 @@ ucompress.css = (source, dest) => new Promise((res, rej) => {
   });
 });
 
+/**
+ * Copy a source file into a destination.
+ * @param {string} source The source file to copy.
+ * @param {string} dest The destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.copy = (source, dest) => new Promise((res, rej) => {
   copyFile(source, dest, err => {
     err ? rej(err) : res(dest);
   });
 });
 
+/**
+ * Create a file after optimizing via `gifsicle`.
+ * @param {string} source The source GIF file to optimize.
+ * @param {string} dest The optimized destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.gif = (source, dest) => new Promise((res, rej) => {
   execFile(gifsicle, ['-o', dest, source], err => {
     err ? rej(err) : res(dest);
   });
 });
 
+/**
+ * Create a file after minifying it via `html-minifier`.
+ * @param {string} source The source HTML file to minify.
+ * @param {string} dest The minified destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.html = (source, dest) => new Promise((res, rej) => {
   readFile(source, (err, data) => {
     if (err)
@@ -73,14 +103,32 @@ ucompress.html = (source, dest) => new Promise((res, rej) => {
   });
 });
 
+/**
+ * Create a file after optimizing via `jpegtran`.
+ * @param {string} source The source JPG/JPEG file to optimize.
+ * @param {string} dest The optimized destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.jpg = (source, dest) => new Promise((res, rej) => {
   execFile(jpegtran, jpegtranArgs.concat(dest, source), err => {
     err ? rej(err) : res(dest);
   });
 });
 
+/**
+ * Create a file after optimizing via `jpegtran`.
+ * @param {string} source The source JPG/JPEG file to optimize.
+ * @param {string} dest The optimized destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.jpeg = ucompress.jpg;
 
+/**
+ * Create a file after minifying it via `uglify-es`.
+ * @param {string} source The source JS file to minify.
+ * @param {string} dest The minified destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.js = (source, dest) => new Promise((res, rej) => {
   readFile(source, (err, data) => {
     if (err)
@@ -98,6 +146,12 @@ ucompress.js = (source, dest) => new Promise((res, rej) => {
   });
 });
 
+/**
+ * Create a file after optimizing via `pngquant`.
+ * @param {string} source The source PNG file to optimize.
+ * @param {string} dest The optimized destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.png = (source, dest) => new Promise((res, rej) => {
   execFile(pngquant, pngquantArgs.concat(dest, source), err => {
     if (err) {
@@ -110,6 +164,12 @@ ucompress.png = (source, dest) => new Promise((res, rej) => {
   });
 });
 
+/**
+ * Create a file after minifying it via `svgo`.
+ * @param {string} source The source SVG file to minify.
+ * @param {string} dest The minified destination file.
+ * @return {Promise<string>} A promise that resolves with the destination file.
+ */
 ucompress.svg = (source, dest) => new Promise((res, rej) => {
   readFile(source, (err, data) => {
     if (err)
