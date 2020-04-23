97a9ee921059bbff7f9e558caa50ab8949ca2402
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-04-23 16:51:46 +0200

    testing via cluster all options

--- test/express.cjs
+++ test/express.cjs
@@ -1,11 +1,23 @@
+const {fork, isMaster} = require('cluster');
 const {join} = require('path');
+const os = require('os');
 
 const express = require('express');
 const {cdn} = require('../cjs');
 
-const app = express();
-app.use(cdn({
-  source: join(__dirname, 'source'),
-  dest: join(__dirname, 'dest')
-}));
-app.listen(8080);
+const {length} = os.cpus();
+
+if (isMaster) {
+  for (var i = 0; i < length; i++)
+    fork().on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+}
+else {
+  const app = express();
+  app.use(cdn({
+    source: join(__dirname, 'source'),
+    dest: join(__dirname, 'dest')
+  }));
+  app.listen(8080);
+}
--- test/static.cjs
+++ test/static.cjs
@@ -1,8 +1,21 @@
+const {fork, isMaster} = require('cluster');
 const {join} = require('path');
+const os = require('os');
+
 const express = require('express');
 const compression = require('compression');
 
-const app = express();
-app.use(compression());
-app.use(express.static(join(__dirname, 'source')));
-app.listen(8080);
+const {length} = os.cpus();
+
+if (isMaster) {
+  for (var i = 0; i < length; i++)
+    fork().on('exit', (worker, code, signal) => {
+      console.log(`worker ${worker.process.pid} died`);
+    });
+}
+else {
+  const app = express();
+  app.use(compression());
+  app.use(express.static(join(__dirname, 'source')));
+  app.listen(8080);
+}
