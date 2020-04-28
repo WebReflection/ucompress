0b3f9abe735c5abe827cf05c76885645b3f1d183
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2020-04-28 17:14:05 +0200

    using `sharp` to grant maximum images sizes

--- cjs/png.js
+++ cjs/png.js
@@ -1,12 +1,36 @@
 'use strict';
 const {execFile} = require('child_process');
-const {copyFile} = require('fs');
+const {copyFile, unlink} = require('fs');
 
 const pngquant = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('pngquant-bin'));
+const sharp = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('sharp'));
 
 const headers = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('./headers.js'));
 
+const fit = sharp.fit.inside;
 const pngquantArgs = ['--skip-if-larger', '--speed', '1', '-f', '-o'];
+const withoutEnlargement = true;
+
+const optimize = (source, dest, options) => new Promise((res, rej) => {
+  execFile(pngquant, pngquantArgs.concat(dest, source), err => {
+    if (err) {
+      copyFile(source, dest, err => {
+        if (err)
+          rej(err);
+        else if (options.createFiles)
+          headers(source, dest, options.headers)
+            .then(() => res(dest), rej);
+        else
+          res(dest);
+      });
+    }
+    else if (options.createFiles)
+      headers(source, dest, options.headers)
+        .then(() => res(dest), rej);
+    else
+      res(dest);
+  });
+});
 
 /**
  * Create a file after optimizing via `pngquant`.
@@ -17,22 +41,26 @@ const pngquantArgs = ['--skip-if-larger', '--speed', '1', '-f', '-o'];
  */
 module.exports = (source, dest, /* istanbul ignore next */ options = {}) =>
   new Promise((res, rej) => {
-    execFile(pngquant, pngquantArgs.concat(dest, source), err => {
-      if (err) {
-        copyFile(source, dest, err => {
-          if (err)
-            rej(err);
-          else if (options.createFiles)
-            headers(source, dest, options.headers)
-              .then(() => res(dest), rej);
-          else
-            res(dest);
-        });
-      }
-      else if (options.createFiles)
-        headers(source, dest, options.headers)
-          .then(() => res(dest), rej);
-      else
-        res(dest);
-    });
+    const {maxWidth: width, maxHeight: height} = options;
+    if (width || height) {
+      sharp(source)
+        .resize({width, height, fit, withoutEnlargement})
+        .toFile(`${dest}.resized.png`)
+        .then(
+          () => optimize(`${dest}.resized.png`, dest, options).then(
+            () => {
+              unlink(`${dest}.resized.png`, err => {
+                /* istanbul ignore if */
+                if (err) rej(err);
+                else res(dest);
+              });
+            },
+            rej
+          ),
+          rej
+        )
+      ;
+    }
+    else
+      optimize(source, dest, options).then(res, rej);
   });
--- test/index.js
+++ test/index.js
@@ -1,5 +1,5 @@
 const {readdir} = require('fs');
-const {basename, extname, join} = require('path');
+const {basename, join} = require('path');
 const {log, ok, error} = require('essential-md');
 const ucompress = require('../cjs');
 
@@ -22,115 +22,141 @@ readdir(join(__dirname, 'source'), (_, files) => {
   }
 });
 
-
-
-// regular test for each kind of file + extras
-readdir(join(__dirname, 'source'), (_, files) => {
-  for (const file of files) {
-    ucompress(
-      join(__dirname, 'source', file),
-      join(__dirname, 'dest', file),
-      {createFiles: true}
-    )
-    .then(dest => ok(`\`${basename(dest)}\``))
-    .catch(err => {
-      error`\`\`\`${err}\`\`\``;
-      process.exit(1);
+setTimeout(
+  () => {
+    // regular test for each kind of file + extras
+    readdir(join(__dirname, 'source'), (_, files) => {
+      for (const file of files) {
+        ucompress(
+          join(__dirname, 'source', file),
+          join(__dirname, 'dest', file),
+          {createFiles: true}
+        )
+        .then(dest => ok(`\`${basename(dest)}\``))
+        .catch(err => {
+          error`\`\`\`${err}\`\`\``;
+          process.exit(1);
+        });
+      }
     });
-  }
-});
+    setTimeout(
+      () => {
+        // regular test for each kind of file + extras + maxWidth
+        readdir(join(__dirname, 'source'), (_, files) => {
+          for (const file of files) {
+            ucompress(
+              join(__dirname, 'source', file),
+              join(__dirname, 'dest', file),
+              {
+                createFiles: true,
+                maxWidth: 320,
+                maxHeight: 320
+              }
+            )
+            .then(dest => ok(`\`${basename(dest)}\``))
+            .catch(err => {
+              error`\`\`\`${err}\`\`\``;
+              process.exit(1);
+            });
+          }
+        });
+        setTimeout(
+          () => {
+            // wrong source files
+            readdir(join(__dirname, 'source'), (_, files) => {
+              for (const file of files) {
+                ucompress(
+                  join(__dirname, 'source', `no-${file}`),
+                  join(__dirname, 'dest', `no-${file}`)
+                )
+                .then(dest => {
+                  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+                  process.exit(1);
+                })
+                .catch(() => ok`\`no-${file}\` failed`);
+              }
+            });
+            setTimeout(lastChecks, 1000);
+          },
+          1000
+        );
+      },
+      1000
+    );
+  },
+  1000
+);
 
+function lastChecks() {
+  // wrong destination folders
+  ucompress.css(
+    join(__dirname, 'source', 'index.css'),
+    join(__dirname, 'shenanigans', `index.css`)
+  )
+  .then(dest => {
+    error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+    process.exit(1);
+  })
+  .catch(() => ok`wrong CSS destination fails as expected`);
 
+  ucompress.js(
+    join(__dirname, 'source', 'index.js'),
+    join(__dirname, 'shenanigans', `index.js`)
+  )
+  .then(dest => {
+    error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+    process.exit(1);
+  })
+  .catch(() => ok`wrong JS destination fails as expected`);
 
-// wrong source files
-readdir(join(__dirname, 'source'), (_, files) => {
-  for (const file of files) {
-    ucompress(
-      join(__dirname, 'source', `no-${file}`),
-      join(__dirname, 'dest', `no-${file}`)
-    )
-    .then(dest => {
-      error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-      process.exit(1);
-    })
-    .catch(() => ok`\`no-${file}\` failed`);
-  }
-});
+  ucompress.html(
+    join(__dirname, 'source', 'index.html'),
+    join(__dirname, 'shenanigans', `index.html`)
+  )
+  .then(dest => {
+    error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+    process.exit(1);
+  })
+  .catch(() => ok`wrong HTML destination fails as expected`);
 
+  ucompress.svg(
+    join(__dirname, 'source', 'benja-dark.svg'),
+    join(__dirname, 'shenanigans', `benja-dark.svg`)
+  )
+  .then(dest => {
+    error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+    process.exit(1);
+  })
+  .catch(() => ok`wrong SVG destination fails as expected`);
 
+  // other kind of failures
+  ucompress.js(
+    join(__dirname, 'source', 'favicon.ico'),
+    join(__dirname, 'dest', `shenanigans.js`)
+  )
+  .then(dest => {
+    error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+    process.exit(1);
+  })
+  .catch(() => ok`bad JS fails as expected`);
 
-// wrong destination folders
-ucompress.css(
-  join(__dirname, 'source', 'index.css'),
-  join(__dirname, 'shenanigans', `index.css`)
-)
-.then(dest => {
-  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-  process.exit(1);
-})
-.catch(() => ok`wrong CSS destination fails as expected`);
+  ucompress.html(
+    join(__dirname, 'source', 'favicon.ico'),
+    join(__dirname, 'dest', `shenanigans.html`)
+  )
+  .then(dest => {
+    error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+    process.exit(1);
+  })
+  .catch(() => ok`bad HTML fails as expected`);
 
-ucompress.js(
-  join(__dirname, 'source', 'index.js'),
-  join(__dirname, 'shenanigans', `index.js`)
-)
-.then(dest => {
-  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-  process.exit(1);
-})
-.catch(() => ok`wrong JS destination fails as expected`);
-
-ucompress.html(
-  join(__dirname, 'source', 'index.html'),
-  join(__dirname, 'shenanigans', `index.html`)
-)
-.then(dest => {
-  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-  process.exit(1);
-})
-.catch(() => ok`wrong HTML destination fails as expected`);
-
-ucompress.svg(
-  join(__dirname, 'source', 'benja-dark.svg'),
-  join(__dirname, 'shenanigans', `benja-dark.svg`)
-)
-.then(dest => {
-  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-  process.exit(1);
-})
-.catch(() => ok`wrong SVG destination fails as expected`);
-
-
-
-// other kind of failures
-ucompress.js(
-  join(__dirname, 'source', 'favicon.ico'),
-  join(__dirname, 'dest', `shenanigans.js`)
-)
-.then(dest => {
-  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-  process.exit(1);
-})
-.catch(() => ok`bad JS fails as expected`);
-
-ucompress.html(
-  join(__dirname, 'source', 'favicon.ico'),
-  join(__dirname, 'dest', `shenanigans.html`)
-)
-.then(dest => {
-  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-  process.exit(1);
-})
-.catch(() => ok`bad HTML fails as expected`);
-
-ucompress.png(
-  join(__dirname, 'source', 'shenanigans.png'),
-  join(__dirname, 'dest', `shenanigans.png`)
-)
-.then(dest => {
-  error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
-  process.exit(1);
-})
-.catch(() => ok`bad PNG fails as expected`);
-
-//*/
+  ucompress.png(
+    join(__dirname, 'source', 'shenanigans.png'),
+    join(__dirname, 'dest', `shenanigans.png`)
+  )
+  .then(dest => {
+    error`\`\`\`${dest}\`\`\` did not exists, this should've failed!`;
+    process.exit(1);
+  })
+  .catch(() => ok`bad PNG fails as expected`);
+}
